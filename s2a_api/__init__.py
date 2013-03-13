dcf517c89ee404db04fa2299e3aab3e47012ef90
mike <michal@duszyk.com>
2013-03-13 13:50:39 +0100

    added api object

--- s2a_api/__init__.py
+++ s2a_api/__init__.py
@@ -4,5 +4,5 @@ from quote import QuoteService
 class S2aApi(object):
     
     def __init__(self, api_url):
-        self.quote = QuoteService(api_url)
+        self.quote_service = QuoteService(api_url)
         # TODO add more
