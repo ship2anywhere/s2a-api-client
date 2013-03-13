15d0ce6d52f4a57258aad204127987ebc297357b
mike <michal@duszyk.com>
2013-03-13 12:57:41 +0100

    project structure

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import sys
+import glob
+import os.path
+from setuptools import setup, find_packages
+
+MYDIR = os.path.dirname(os.path.abspath(__file__))
+
+def find_pyfiles(localdir=''):
+    path = os.path.join(MYDIR, localdir)
+    pattern = os.path.join(path, '*.py')
+    return glob.glob(pattern)
+
+# find project packages
+packages = find_packages(exclude=["test*"])
+
+setup(
+    name = 's2a_api',
+    version = '1.0',
+    description = 'Ship2Anywhere API client library',
+    package_dir = {'s2a_api': 's2a_api'},
+    packages = packages,
+    install_requires = [
+        #'beautifulsoup4==4.1.3',
+        #'tornado==2.4',
+    ],
+)
