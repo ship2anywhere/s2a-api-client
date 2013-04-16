2a8da867928a5e4ea02b74d33005c947b7f813b8
Marek W <marek.wolczanski@gmail.com>
2013-04-16 22:43:25 +0200

    common api_call for get_quote

--- s2a_api/quote.py
+++ s2a_api/quote.py
@@ -1,9 +1,7 @@
 import logging
 import json
-import requests
-import exception
-from error_handler import handle_error
 from util import append_slash
+from util import api_call
 
 LOG = logging.getLogger(__name__)
 
@@ -18,20 +16,4 @@ class QuoteService(object):
         req = {"request": request}
         headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
         
-        if LOG.isEnabledFor(logging.DEBUG):
-            LOG.debug("Request URL: %s ; Request Data: %s" % (self.api_url, req))
-        
-        resp = requests.post(url=self.api_url, data=json.dumps(req), headers=headers, verify = verify_cert)
-
-        if LOG.isEnabledFor(logging.DEBUG):
-            LOG.debug("Status Code: %s; Response Body: %s" % (resp.status_code, resp.text))
-        
-        handle_error(resp)
-
-        try:
-            json_dict = resp.json()
-            return json_dict
-        except ValueError:
-            LOG.exception("API returned corrupted message")
-            raise S2aApiException("API returned corrupted message: " + str(e))
-
+        return api_call("post", self.api_url, data=json.dumps(req), headers=headers)
--- s2a_api/util.py
+++ s2a_api/util.py
@@ -1,5 +1,36 @@
 # S2A API Client common functions
 
+import requests
+import logging
+import json
+import exception
+from error_handler import handle_error
+
+LOG = logging.getLogger(__name__)
+
+def api_call(method, url, data='', headers={}, token=None, 
+        verify_cert=False, success_codes=(200,)):
+    """ General function which can call any S2A API service """
+    
+    if token:
+        headers["Authorization"] = "Bearer %s" % token
+        
+    if LOG.isEnabledFor(logging.DEBUG):
+        LOG.debug("Request URL: %s ; method: %s ; Request Data: %s ; Request Token: %s" \
+            % (url, method, data, token))
+            
+    resp = requests.request(method, url, data=data, headers=headers, verify=verify_cert)
+    
+    if LOG.isEnabledFor(logging.DEBUG):
+        LOG.debug("Status Code: %s; Response Body: %s" % (resp.status_code, resp.text))
+        
+    handle_error(resp, success_codes)
+    
+    try:
+        return resp.json()
+    except ValueError as e:
+        LOG.exception("API returned corrupted message")
+        raise S2aApiException("API returned corrupted message: %s" % e)
 
 def append_slash(url):
     """ Function which return URL ended with the slash "/" """
