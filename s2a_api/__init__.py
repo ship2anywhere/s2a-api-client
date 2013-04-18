752c25686d708957c761e195f6bb61f7074daed3
mike <michal@duszyk.com>
2013-04-18 22:12:31 +0200

    improved S2aApi class

--- s2a_api/__init__.py
+++ s2a_api/__init__.py
@@ -1,12 +1,18 @@
 from quote import QuoteService
-
+from order import OrderService
+from doc import DocumentService
+from track import TrackService
+from util import append_slash
 
 class S2aApi(object):
+    """ This class allows to create all API service objects in single place.
+        Simplifies instantiation and access to API services.
+    """
     
-    def __init__(self, api_url):
-        self.api_url = api_url
-        self.quote_service = QuoteService(api_url)
-        self.order_servive = OrderService(api_url)
-        self.doc_servive = OrderService(api_url)
-        self.track_servive = OrderService(api_url)
-        # TODO add more
+    def __init__(self, api_url, verify_cert = False):
+        self.api_url = append_slash(api_url)
+        self.verify_cert= verify_cert
+        self.quote_service = QuoteService(api_url, verify_cert)
+        self.order_servive = OrderService(api_url, verify_cert)
+        self.doc_servive = DocumentService(api_url, verify_cert)
+        self.track_servive = TrackService(api_url, verify_cert)
